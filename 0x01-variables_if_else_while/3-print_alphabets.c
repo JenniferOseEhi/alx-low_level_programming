40ceed4e86ae43a9dd0e6219397d402291e8beca
JenniferOseEhi <jenniferoseehi@gmail.com>
2022-03-14 00:50:52 -0700

    adding 3-print_alphabet.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -4,7 +4,7 @@
 
 /**
 
- * main - prints the alphabet in lowercase,
+ * main - prints the alphabet in lowercase, and then in uppercase,
 
  * followed by a new line
 
@@ -16,17 +16,17 @@ int main(void)
 
 {
 
-	char ch;
+	int ch;
 
 
 
 	for (ch = 'a'; ch <= 'z'; ch++)
 
-	{
-
 		putchar(ch);
 
-	}
+	for (ch = 'A'; ch <= 'Z'; ch++)
+
+		putchar(ch);
 
 	putchar('\n');
 
