9a418317ea69787117a30f2cd4bedf7b77fc12f1
Jennifer Ose Ehiagbonare <68024023+JenniferOseEhi@users.noreply.github.com>
2022-03-16 18:19:19 +0100

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,7 +7,7 @@
  * Return: Always 0
 */
 
-void print_times_table(int n)
+void times_table(int n)
 {
 int i, j, k;
 if (n >= 0 && n <= 15)
@@ -44,8 +44,6 @@ _putchar((k % 10) + '0');
 }
 }
 _putchar('\n');
-  
-return (0);
 }
 }
 }
