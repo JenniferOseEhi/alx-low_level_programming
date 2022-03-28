58400b48eade7e4d992950bebeabd4d52003dfb9
Jennifer Ose Ehiagbonare <68024023+JenniferOseEhi@users.noreply.github.com>
2022-03-28 15:50:19 +0100

    Update 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,22 +10,21 @@
 char *_strchr(char *s, char c)
 {
 
-	while (*s != '\0') /*Declaring WHILE*/
-	{
-		if (*s == c) /*if s ==  c*/
-		{
-			return (s); /*return s*/
-		}
+while (*s != '\0') /*Declaring WHILE*/
+{
+if (*s == c) /*if s ==  c*/
+{
+return (s); /*return s*/
+}
 
-		++s;
-	}
+++s;
+}
 
-	if (*s == c)
-	{
-		return (s);
+if (*s == c)
+{
+return (s);
 
-	}
+}
 
-
-	return (0);/* values null*/
+return (0);/* values null*/
 }
